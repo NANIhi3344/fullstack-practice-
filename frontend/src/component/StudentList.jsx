815c025ea551fbb3037f0c2a07215496adf7c40b
NANI <@github.comgit config --global user.email>
2025-03-24 12:42:12 +0530

    good

---
+++ frontend/src/component/StudentList.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import StudentItem form "./StudentItem.jsx";    
+import "./../App.css";
+
+const StudentData = [
+    { id: 1, name: "Alice", age: 20, grade: "A"},
+    { id: 2, name: "Bob", age: 20, grade: "B"},
+    { id: 3, name: "Charlie", age: 21, grade: "A-"}
+];
+
+const StudentList = () => {
+    return (
+        <div className = 'student-list'>
+        <h2> Student List </h2>
+        <ul>
+        {StudentData.map((student) => (
+            <StudentItem key={student.id} name={student.name} age={student.age} grade={student.grade}/>
+        ))}
+        </ul>
+        </div>
+    );
+};
+
+export default StudentList;
+
